7a6ecab902d227151448ca15a5cce615bb1b39e9
hectorhauffmann <111776996+hectorhauffmann@users.noreply.github.com>
2022-09-04 18:48:57 -0300

    Delete conexion.php

--- conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host= "127.0.0.1";//La ip de nuestra maquina principal
-$usuario="root";//usuario que configuramos MySQL Workbench
-$clave="123123123";//contrasena que configuramos MySQL Workbench
-$bd="semana9";//Nombre de Base de datos
-
-$conexion= mysqli_connect($host,$usuario,$clave,$bd);
-?>
